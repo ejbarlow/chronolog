31ba20795b40583b88af2a70a46e4b3d30b52bd7
ejbarlow <85363023+ejbarlow@users.noreply.github.com>
2021-08-12 22:08:54 +0100

    Added patch file and updated README.md (#5)

---
+++ utils/patchWebpackConfig.js
@@ -0,0 +1,22 @@
+/* Patch react-scripts' webpack.config.js
+ * Prevents hanging due to duplicated promise
+ * https://github.com/pixelkritzel/savages/blob/master/patchWebpackConfig.js */
+
+const fs = require("fs").promises;
+
+async function patch() {
+  let webpackConfig = await fs.readFile(
+    "./node_modules/react-scripts/config/webpack.config.js",
+    "utf-8"
+  );
+  webpackConfig = webpackConfig.replace(
+    "new ForkTsCheckerWebpackPlugin({",
+    "new ForkTsCheckerWebpackPlugin({useTypescriptIncrementalApi: false,"
+  );
+  await fs.writeFile(
+    "./node_modules/react-scripts/config/webpack.config.js",
+    webpackConfig,
+    "utf-8"
+  );
+}
+patch();
