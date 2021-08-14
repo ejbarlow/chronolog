1d73157b9da39819f7b698938632587cc5ff4942
EJ Barlow <ed@ejbarlow.co.uk>
2021-08-15 00:23:34 +0100

    Node scripts to `.js`.

---
+++ utils/patchWebpackConfig.js
@@ -0,0 +1,44 @@
+/* Patch react-scripts' webpack.config.js
+ * Prevents hanging due to duplicated promise
+ * https://github.com/pixelkritzel/savages/blob/master/patchWebpackConfig.js */
+
+import { promises as fs } from "fs";
+
+import log from "./log.mjs";
+
+async function patch() {
+  let webpackConfig = await fs
+    .readFile("./node_modules/react-scripts/config/webpack.config.js", "utf-8")
+    .catch((err) => {
+      console.error(
+        `\n${String.fromCodePoint(0x274c)} Error reading webpack.config.js`,
+        err
+      );
+    });
+  webpackConfig = webpackConfig.replace(
+    "new ForkTsCheckerWebpackPlugin({",
+    "new ForkTsCheckerWebpackPlugin({useTypescriptIncrementalApi: false,"
+  );
+  await fs
+    .writeFile(
+      "./node_modules/react-scripts/config/webpack.config.js",
+      webpackConfig,
+      "utf-8"
+    )
+    .then(() => {
+      console.log();
+      log(
+        `${String.fromCodePoint(
+          0x2705
+        )} "./node_modules/react-scripts/config/webpack.config.js" patched`,
+        "fgGreen"
+      );
+    })
+    .catch((err) => {
+      console.error(
+        `\n${String.fromCodePoint(0x274c)} Error writing webpack.config.js`,
+        err
+      );
+    });
+}
+patch();
